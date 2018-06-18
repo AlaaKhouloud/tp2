980a021744ddf2d48943337c246b0cd5cb8bf7dc
AlaaKhouloud <khouloud.alaa.est@gmail.com>
2018-06-18 04:15:35 +0200

    tp2

---
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Form()
+        {
+            string nom = Request.Form["noms"].ToString();
+            return RedirectToAction("About", "Home",nom);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class User
+    {
+        public string nom { get; set; }
+        public string prenom { get; set; }
+
+        public User(string nom, string prenom)
+        {
+            this.nom = nom;
+            this.prenom = prenom;
+        }
+    }
+}
